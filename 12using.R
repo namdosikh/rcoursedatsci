a7737bad0ae8b705f071ad182dd3b069e220d841
Muhammad Rasikh <79510536+namdosikh@users.noreply.github.com>
2021-12-26 15:28:00 +0700

    Add files via upload

---
+++ 12using.R
@@ -0,0 +1,19 @@
+a <- 10
+b <- 5
+
+c <- a + b
+c
+
+# variable
+var1 <- 3
+var2 <- 4
+
+result <- var1 / var2
+result
+
+answer <- sqrt(var2)
+
+greet <- "Hei"
+name <- "Bob"
+message <- paste(greet, name)
+message
---
+++ 13logical.R
@@ -0,0 +1,15 @@
+4 < 5
+10 > 100
+4 == 5
+
+2 | 3
+2 & 3
+isTRUE(5)
+
+result <- !F
+res <- !(5 > 1)
+
+result | res
+result & res
+
+isTRUE(res)
